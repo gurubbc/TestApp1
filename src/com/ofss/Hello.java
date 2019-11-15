ff64ee633eb85ab78744d6d6ac95f20d2b88ad6d
Guru <gurumurthy.ramamurthy@gmail.com>
2019-11-15 08:58:16 +0530

    kanna2

--- src/com/ofss/Hello.java
+++ src/com/ofss/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello1 4 ");
 		System.out.println("Now from github");
-		System.out.println("kanna ");
+		System.out.println("kanna 2");
 		
 
 	}

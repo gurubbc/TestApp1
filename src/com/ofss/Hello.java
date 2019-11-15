55023d6956d890df21586ddb0060bbc7a804cb75
Guru <gurumurthy.ramamurthy@gmail.com>
2019-11-15 08:44:13 +0530

    from github

--- src/com/ofss/Hello.java
+++ src/com/ofss/Hello.java
@@ -3,7 +3,8 @@ package com.ofss;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello1 3 ");
+		System.out.println("hello1 4 ");
+		System.out.println("Now from github");
 		
 
 	}
